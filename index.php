c1efd3632d6b524573a2ddba9dda4b9e9e9a3457
Pranjal kalekar <60231901+Pranjal10052000@users.noreply.github.com>
2022-03-26 10:15:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,476 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AICTE-Event Organizer Website Design </title>
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- header section starts  -->
+
+<header class="header">
+    <img src="images\logo.png" >
+    <a href="#" class="logo"><span>A</span>ICTE</a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#Activities">Activities</a>
+        <a href="#service">service</a>
+        <a href="#about">about</a>
+        <a href="#gallery">gallery</a>
+        <a href="#price">price</a>
+        <a href="#review">review</a>
+        <a href="#contact">contact</a>
+        <a href="register.php">Sign Up</a>
+    </nav>
+
+    <div id="menu-bars" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<!-- <section class="home" id="home">
+
+    <div class="content">
+        <h3>Knowledge is like garden <span> LETS HARVEST IT TOGETHER WITH AICTE </span></h3>
+
+    </div>
+
+    <div class="swiper-container home-slider">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><img src="images\i1.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images\i2.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images\i3.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images\i4.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images\i5.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images\i6.jpg" alt=""></div>
+        </div>
+    </div>
+
+</section> -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>Knowledge is like garden <span> LETS HARVEST IT TOGETHER WITH AICTE </span></h3>
+        
+    </div>
+
+    <div class="swiper-container home-slider">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><img src="images\i1.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images\i2.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images\i3.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images\i4.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images\i5.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images\i6.jpg" alt=""></div>
+        </div>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- service section starts  -->
+
+<section class="service" id="service">
+
+    <h1 class="heading"> Select <span>Activities</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <i class="fas fa-calendar-alt"></i>
+          <a href="hackathon.php">  <h3>Hackathon</h3></a>
+            <div class="overlay">
+              <p>Hackathons which help you put your coding skills to work. Solve interesting business problems and real-world challenges. Register now.</p>
+
+            </div>
+
+        </div>
+
+        <div class="box">
+            <i class="fas fa-bookmark"></i>
+            <a href="ted-talks.php">  <h3>Ted-Talks</h3></a>
+            <p> Our goal is to inform and educate global audiences in an accessible way.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-dice-five"></i>
+            <a href="workshops.php">  <h3>Workshops</h3></a>
+            <p>Here we conduct variour workshops for enhancement of your experience and widening your knowledge.</p>
+        </div>
+
+
+        <div class="box">
+            <i class="fas fa-bullhorn"></i>
+            <a href="seminars.php">  <h3>Seminars</h3></a>
+            <p>Ofter seminars are conducted by various motivational speakers who are experienced inn their respective fields to share their knowledge and experience.</p>
+        </div>
+
+    </div>
+
+</section>
+
+
+
+<section class="service" id="service">
+
+    <h1 class="heading"> our <span>services</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <i class="fas fa-map-marker-alt"></i>
+            <h3>venue selection</h3>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-envelope"></i>
+            <h3>invitation card</h3>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-badge-check"></i>
+            <h3>technical expertise</h3>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-coffee"></i>
+            <h3>food and drinks</h3>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- service section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+<h1 class="heading"><span>about</span> us </h1>
+
+<div class="row">
+
+    <div class="image">
+        <img src="images/about-img.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>We Are <span class="heading">Passionate</span>.<br> We Are <span class="heading">Committed</span>.<br>We Are <span class="heading">Creative</span>.</h3>
+        <p>All India Council for Technical Education (AICTE) was set up in November 1945 as a national-level apex advisory body to conduct a survey on the facilities available for technical education and to promote development in the country in a coordinated and integrated manner. And to ensure the same, as stipulated in the National Policy of Education (1986), AICTE was vested with:
+<br>
+</p>
+ <p>• Statutory authority for planning, formulation, and maintenance of norms & standards
+   <br>
+ • Quality assurance through accreditation
+ <br>
+ • Funding in priority areas, monitoring, and evaluation
+ <br>
+ • Maintaining parity of certification & awards
+ <br>
+ • The management of technical education in the country</p>
+</p>
+
+    </div>
+
+</div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- gallery section starts  -->
+
+<section class="gallery" id="gallery">
+
+    <h1 class="heading">our <span>gallery</span></h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images\img1.jpg" alt="">
+            <h3 class="title">photos and events</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images\img2.jpg" alt="">
+            <h3 class="title">photos and events</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images\img3.jpg" alt="">
+            <h3 class="title">photos and events</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images\img4.jpg" alt="">
+            <h3 class="title">photos and events</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images\img5.jpg" alt="">
+            <h3 class="title">photos and events</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images\img6.jpg" alt="">
+            <h3 class="title">photos and events</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images\img7.jpg" alt="">
+            <h3 class="title">photos and events</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images\img8.jpg" alt="">
+            <h3 class="title">photos and events</h3>
+            <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-share"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+        </div>
+
+      </div>
+
+    </div>
+
+</section>
+
+<!-- gallery section ends -->
+
+<!-- price section starts  -->
+
+
+<!-- price section ends -->
+
+<!-- review section starts  -->
+
+<section class="reivew" id="review">
+
+    <h1 class="heading">client's <span>review</span></h1>
+
+    <div class="review-slider swiper-container">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide box">
+                <i class="fas fa-quote-right"></i>
+                <div class="user">
+                    <img src="images\p3.jpeg" alt="">
+                    <div class="user-info">
+                        <h3>Shreya Kapoor</h3>
+                        <span>happy clients</span>
+                    </div>
+                </div>
+                <p>They surpassed our expectations and provided so much value to us."The team is always well structured, organized, flexible, and able to deliver key milestones in the project plan."</p>
+            </div>
+
+            <div class="swiper-slide box">
+                <i class="fas fa-quote-right"></i>
+                <div class="user">
+                    <img src="images\p2.jpeg" alt="">
+                    <div class="user-info">
+                        <h3>Deepika Khowala</h3>
+                        <span>happy clients</span>
+                    </div>
+                </div>
+                <p>"We are very satisfied with the project so far and believe they have done an outstanding job! “I’m most impressed with boolean Digital Marketing & PR’s creativity and the quality of their work.”"</p>
+            </div>
+
+            <div class="swiper-slide box">
+                <i class="fas fa-quote-right"></i>
+                <div class="user">
+                    <img src="images\p1.jpeg" alt="">
+                    <div class="user-info">
+                        <h3>Rushi Singh</h3>
+                        <span>happy clients</span>
+                    </div>
+                </div>
+                <p>"I am impressed with quickly they moved and the fact that quality was not compromised.Their capacity to adapt to specific or special contexts, companies, and situations is impressive"</p>
+            </div>
+
+            <div class="swiper-slide box">
+                <i class="fas fa-quote-right"></i>
+                <div class="user">
+                    <img src="images\p4.jpeg" alt="">
+                    <div class="user-info">
+                        <h3>Kunal Mittal</h3>
+                        <span>happy clients</span>
+                    </div>
+                </div>
+                <p>“They understand our needs and know how to engage with media outlets that are relevant to our industry.They’re well-organized and super easy to work with.”</p>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+            </div>
+
+
+<!-- review section ends -->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+    <h1 class="heading"> <span>contact</span> us </h1>
+
+    <form action="mailto:poojapawar.mng@gmail.com " method="post" enctype="text/plain">
+        <div class="inputBox">
+            <input type="text" placeholder="name">
+            <input type="email" placeholder="email">
+        </div>
+        <div class="inputBox">
+            <input type="number" placeholder="number">
+            <input type="text" placeholder="subject">
+        </div>
+        <textarea name="" placeholder="your message" id="" cols="30" rows="10"></textarea>
+        <input type="submit" value="send message" class="btn">
+    </form>
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>branches</h3>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> mumbai </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> jogeshwari </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> goregaon </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> navi mumbai </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> andheri </a>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#home"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="#service"> <i class="fas fa-arrow-right"></i> service </a>
+            <a href="#"><i class="fas fa-arrow-right"></i>Activities</a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> about </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> gallery </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> price </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> reivew </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> contact </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> 011‑26131498 </a>
+            <a href="#"> <i class="fas fa-phone"></i> 011‑26131498 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> 	ms@aicte-india.org </a>
+            <a href="#"> <i class="fas fa-envelope"></i> pstoms@aicte-india.org  </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/OfficialAICTE/" target="_blank"> <i class="fab fa-facebook-f" ></i> facebook </a>
+            <a href="https://twitter.com/AICTE_INDIA?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"> <i class="fab fa-twitter"></i> twitter </a>
+
+            <a href="https://www.linkedin.com/company/aicteindia/?originalSubdomain=in" target="_blank"> <i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>Boolean Autocrats</span> | all rights reserved </div>
+
+</section>
+
+<!-- footer section ends -->
+
+<!-- theme toggler  -->
+
+<div class="theme-toggler">
+
+    <div class="toggle-btn">
+        <i class="fas fa-cog"></i>
+    </div>
+
+    <h3>choose color</h3>
+
+    <div class="buttons">
+        <div class="theme-btn" style="background: #3867d6;"></div>
+        <div class="theme-btn" style="background: #f7b731;"></div>
+        <div class="theme-btn" style="background: #ff0033;"></div>
+        <div class="theme-btn" style="background: #20bf6b;"></div>
+        <div class="theme-btn" style="background: #fa8231;"></div>
+        <div class="theme-btn" style="background: #FC427B;"></div>
+    </div>
+
+</div>
+
+
+
+
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
